65dd1dc51bb08a5afc07ce2e5fe9f31053b03757
Armank <kulbekov95@gmail.com>
2024-12-16 23:32:53 +0200

    cleanup

--- src/database/queries/score.ts
+++ src/database/queries/score.ts
@@ -22,23 +22,6 @@ export async function findTicketsWithCategories(
   startDate: string,
   endDate: string
 ) {
-  // return knex.raw(
-  //   `
-  //   SELECT
-  //   t.id AS ticket_id,
-  //   r.id AS rating_id,
-  //   r.rating AS rating_value,
-  //   rc.name AS rating_category_name
-  //   FROM tickets t
-  //   JOIN ratings r ON (t.id = r.ticket_id)
-  //   JOIN rating_categories rc ON (r.rating_category_id = rc.id)
-  //   WHERE t.created_at >= ?
-  //   AND t.created_at <= ?
-  //   ORDER BY t.created_at
-  // `,
-  //   [startDate, endDate]
-  // );
-
   return knex.raw(
     `
     SELECT
